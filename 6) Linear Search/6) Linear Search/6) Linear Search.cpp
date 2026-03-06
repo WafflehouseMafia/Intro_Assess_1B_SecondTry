9e98436bf77515e87bc96dadfd9a7a4ca70073d6
WafflehouseMafia <lachlanfudge2002@gmail.com>
2026-03-06 14:53:05 +1100

    Task 6 int main() and asserts
    The task says at the bottom to tell the user where the value is at in the array, so cout seems appropriate here.

--- 6) Linear Search/6) Linear Search/6) Linear Search.cpp
+++ 6) Linear Search/6) Linear Search/6) Linear Search.cpp
@@ -16,3 +16,42 @@ int IntegerSearch(int targetValue, int array[], int arraySize)
 	}
 	return -1; //Just as the task says, if we don't find it then return -1.
 }
+
+int main()
+{
+	const int arraySize = 20;
+	int array[arraySize] = { 67,13,3,89,43,2,19,71,5,61,97,7,37,31,17,11,83,53,23,29 };
+
+	int targetValue = 3;
+	int arrayPosition = IntegerSearch(targetValue, array, arraySize);
+	assert(arrayPosition == 2); //3 is at the third position, so value 2. simple enough
+	if (arrayPosition > -1)
+		cout << "Your number was found at position " << arrayPosition << "\n";
+	else
+		cout << "That value was not in the array.\n";
+
+	targetValue = 7;
+	arrayPosition = IntegerSearch(targetValue, array, arraySize);
+	assert(arrayPosition == 11);
+	if (arrayPosition > -1)
+		cout << "Your number was found at position " << arrayPosition << "\n";
+	else
+		cout << "That value was not in the array.\n";
+
+	targetValue = 67;
+	arrayPosition = IntegerSearch(targetValue, array, arraySize);
+	assert(arrayPosition == 0);
+	if (arrayPosition > -1)
+		cout << "Your number was found at position " << arrayPosition << "\n";
+	else
+		cout << "That value was not in the array.\n";
+
+	targetValue = 88;
+	arrayPosition = IntegerSearch(targetValue, array, arraySize);
+	assert(arrayPosition == -1);
+	if (arrayPosition > -1)
+		cout << "Your number was found at position " << arrayPosition << "\n";
+	else
+		cout << "That value was not in the array.\n";
+
+}
