6bd2addfdbd206c811363680ef5b2936875c8773
WafflehouseMafia <lachlanfudge2002@gmail.com>
2026-03-06 17:00:22 +1100

    Task 9
    Just attaching it so I can do it at home

---
+++ 9) Pointers/9) Pointers.cpp
@@ -0,0 +1,15 @@
+// 9) Pointers.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+
+//I'm going to kermit sewerslide
+
+//This is important to learn, as two teachers have said, but oh my god is it hard to understand.
+
+//A very "I get it, but I don't **get** it" typa deal
+
+//Anyway, you wrote down stuff about Pointers on pages 36 - 42 in your notebook.
